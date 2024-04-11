86aa8678be451bb2d539320100f46ad7a327f6a6
ssebass1045 <sebas.22.26@hotmail.com>
2024-04-11 12:59:02 -0500

    homeWork 02 realizada y ocultar el tempData

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const movieContainer = document.getElementById('movie-container');
-  
+
+
+
+    //----------------------------------------------------------------
+  /*
     // Iterar sobre el array tempData y generar las tarjetas de películas
     tempData.forEach(function(movie) {
       // Crear elementos HTML para la tarjeta de película
@@ -31,5 +35,45 @@ document.addEventListener('DOMContentLoaded', function() {
       // Agregar la tarjeta de película al contenedor
       movieContainer.appendChild(movieCard);
     });
+    */
+    //----------------------------------------------------------------
+
+
+    $.get("https://students-api.up.railway.app/movies", (data) => {
+      renderCards(data);
+    })
+
+    const renderCards = (data) => {
+      data.forEach(function(movie) {
+        // Crear elementos HTML para la tarjeta de película
+        const movieCard = document.createElement('div');
+        movieCard.classList.add('movie-card');
+    
+        const title = document.createElement('a');
+        title.href = '#'; // Esto se puede actualizar para redireccionar a la página correspondiente
+        title.textContent = movie.title;
+    
+        const poster = document.createElement('img');
+        poster.src = movie.poster;
+        poster.alt = movie.title;
+  
+        const details = document.createElement('p');
+      details.innerHTML = `<strong>Year:</strong> ${movie.year}<br>
+                           <strong>Director:</strong> ${movie.director}<br>
+                           <strong>Duration:</strong> ${movie.duration}<br>
+                           <strong>Genre:</strong> ${movie.genre.join(', ')}<br>
+                           <strong>Rate:</strong> <span class="rate">${movie.rate}</span>`; // Agregamos una clase para estilizar el rate
+    
+        // Agregar elementos a la tarjeta de película
+        movieCard.appendChild(title);
+        movieCard.appendChild(poster);
+        movieCard.appendChild(details);
+        
+    
+        // Agregar la tarjeta de película al contenedor
+        movieContainer.appendChild(movieCard);
+      });
+    }
+
   });
   
